b46ee84eb2338922334c6f1e47dee20d89f168af
theinhan8 <159587720+theinhan8@users.noreply.github.com>
2025-04-10 23:54:55 -0600

    Update styles.html, email.html, added a js
    Had to change some styles since it was not working with the emails page. Added a warning for when caps lock is on.

--- js/script.js
+++ js/script.js
@@ -8,18 +8,14 @@ function myFunction() {
   }
 }
 
-/*Displays a thank you message*/
-document.addEventListener("DOMContentLoaded", function () {
-  const form = document.querySelector("form");
-  const nameInput = document.getElementById("fullname");
-  form.addEventListener("submit", function (event) {
-    event.preventDefault();
-    const userName = nameInput.value.trim();
-    if (userName) {
-      alert(`Thanks for subscribing, ${userName}!`); 
-    } else {
-      alert("Thanks for subscribing!");
-    }
-    form.reset();
-  });
+/*Detects CapsLock*/
+var input = document.getElementById("email")
+var text = document.getElementById("text")
+input.addEventListener("keyup", function(event) {
+
+if (event.getModifierState("CapsLock")) {
+    text.style.display = "block";
+  } else {
+    text.style.display = "none"
+  }
 });
