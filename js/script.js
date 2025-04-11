ae2304f89755e3a5e3758ad989d94d4155c15ae5
theinhan8 <159587720+theinhan8@users.noreply.github.com>
2025-04-10 22:40:37 -0600

    Update scrpit.js
    added comment for the function

--- js/script.js
+++ js/script.js
@@ -8,20 +8,18 @@ function myFunction() {
   }
 }
 
+/*Displays a thank you message*/
 document.addEventListener("DOMContentLoaded", function () {
   const form = document.querySelector("form");
   const nameInput = document.getElementById("fullname");
   form.addEventListener("submit", function (event) {
     event.preventDefault();
-
     const userName = nameInput.value.trim();
-
     if (userName) {
       alert(`Thanks for subscribing, ${userName}!`); 
     } else {
       alert("Thanks for subscribing!");
     }
-
     form.reset();
   });
 });
